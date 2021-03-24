0d387e5dd7680ed75aa0e4922bf55526c42a848d
Achllle <achille.verheye@gmail.com>
2021-03-24 11:41:44 -0700

    et network interface to wlan0 for upstart script

---
+++ ff_examples_ros1/set_dds_interface.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+source /home/fetch/catkin_ws/devel/setup.bash
+export CYCLONEDDS_URI='<CycloneDDS><Domain><General><NetworkInterfaceAddress>wlan0</NetworkInterfaceAddress></General></Domain></CycloneDDS>'
+
+
+
